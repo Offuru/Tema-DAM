728c6724c5cd6e94e439b512e3b5ae037cd158e8
Offuru <andrei.rozmarin@gmail.com>
2024-11-08 20:28:40 +0200

    added initial project files

---
+++ Sensor Logger/Sensor Logger/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Sensor_Logger
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Sensor Logger/Sensor Logger/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Sensor_Logger
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
